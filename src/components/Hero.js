b52bf2418142ec6b6a0a293b72cdc3d371be8de8
ceimiplace <azamfiredenis@yahoo.com>
2023-02-12 09:49:31 +0200

    finished hero

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import hero from "../images/hero.svg";
+import "./Hero.css";
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero--logo-container">
+        <img
+          src={hero}
+          alt="collection of pictures"
+          className="hero--logo-container--logo"
+        />
+      </div>
+      <h2 className="hero--heading">Online Experiences</h2>
+      <p className="hero--text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </div>
+  );
+}
